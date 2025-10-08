d59b2e8de6c19e85d4935d1686fb0da17440b6f8
JW-Albert <ru04jo30801@gmail.com>
2025-10-08 22:36:17 +0800

    run

--- Exam-2/Q2/compile.sh
+++ Exam-2/Q2/compile.sh
@@ -4,11 +4,11 @@ echo "Removing old server and client..."
 rm -f server client
 
 echo "Compiling server..."
-gcc -o server Server.c
+gcc -o server Server2.c
 echo "Server compiled"
 
 echo "Compiling client..."
-gcc -o client Client.c
+gcc -o client Client2.c
 echo "Client compiled"
 
 echo "Done"
