0292823d63ebb9ca8025d7c76e06e2b40d86c6ee
JW-Albert <ru04jo30801@gmail.com>
2025-10-08 23:23:18 +0800

    Update compile.sh to rename executables to server_exec and client_exec, enhancing clarity and consistency in the build process.

--- Exam-2/Q1V2/compile.sh
+++ Exam-2/Q1V2/compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -f server client
+rm -f server_exec client_exec
 
-gcc -o server Server1.c
+gcc -o server_exec Server1.c
 
-gcc -o client Client1.c
+gcc -o client_exec Client1.c
 
 clear
---
+++ Exam-2/Q1V2/server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./compile.sh
+
+./server_exec
